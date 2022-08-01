d919143dd69421b78c9385c015a4307ad561787a
Ewelina Głębocka <eglebocka95@gmail.com>
2022-08-01 22:31:02 +0200

    Added solution and projects

--- System HR/System HR/Program.cs
+++ System HR/System HR/Program.cs
@@ -16,7 +16,7 @@ namespace System_HR
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run();
         }
     }
 }
